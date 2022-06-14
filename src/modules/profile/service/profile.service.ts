34e3ac71ed9cbea0db7d98b7bc58b176c5104937
Mathias Mogensen <mathiasrieckm@gmail.com>
2022-06-14 22:54:09 +0200

    feat: s3 fix + distribution

--- src/modules/profile/service/profile.service.ts
+++ src/modules/profile/service/profile.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { IProfileRepository, Profile } from '@domain/profile';
+import { ProfileDoesNotExistError } from '../errors';
 
 @Injectable()
 export class ProfileService {
     constructor(private readonly profileRepository: IProfileRepository) {}
 
     async findByIdOrFail(id: string): Promise<Profile> {
-        return this.profileRepository.findOne({ id });
+        const profile = await this.profileRepository.findOne({ id });
+
+        if (!profile) {
+            throw new ProfileDoesNotExistError(id);
+        }
+
+        return profile;
     }
 
     async findByUserIdOrFail(userId: string): Promise<Profile> {
-        return this.profileRepository.findOne({ userId });
+        const profile = await this.profileRepository.findOne({ userId });
+
+        if (!profile) {
+            throw new ProfileDoesNotExistError(userId);
+        }
+
+        return profile;
     }
 }
