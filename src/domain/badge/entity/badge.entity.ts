3b74e1e31f85becf1257787634a4ef4dd4c8ee24
Michael Slattery <michaelslatterydev@gmail.com>
2022-07-17 14:36:11 +0200

    Add badge interface and update crowdaction dto

--- src/domain/badge/entity/badge.entity.ts
+++ src/domain/badge/entity/badge.entity.ts
@@ -1,7 +1,14 @@
-import { AwardTypeEnum, BadgeTierEnum } from '../enum/badge.enum';
+import { AwardTypeEnum, BadgeTierEnum } from '../enum';
+import { IBadge } from '../interface/badge.interface';
 
-export class Badge {
+export class Badge implements IBadge {
     readonly tier: BadgeTierEnum;
     readonly awardType: AwardTypeEnum;
     readonly minimumCheckIns: number;
+
+    constructor(entityLike: IBadge) {
+        this.tier = entityLike.tier;
+        this.awardType = entityLike.awardType;
+        this.minimumCheckIns = entityLike.minimumCheckIns;
+    }
 }
