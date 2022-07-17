1cf85cfdebab932d0bf26adf401fd6057740ff5e
Mathias Mogensen <mathiasrieckm@gmail.com>
2022-07-17 14:37:47 +0200

    feat: update crowdaction status for started/waiting crowdacitons hourly
    Closes: #21

--- src/infrastructure/tasks/crowdaction/update-crowdaction-status.task.ts
+++ src/infrastructure/tasks/crowdaction/update-crowdaction-status.task.ts
@@ -1,13 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Logger } from '@common/logger';
+import { ICQRSHandler } from '@common/cqrs';
+import { ListCrowdActionsQuery, UpdateCrowdActionStatusesCommand } from '@modules/crowdaction';
+import { CrowdAction, CrowdActionStatusEnum } from '@domain/crowdaction';
 
+const FILTER = { status: { in: [CrowdActionStatusEnum.STARTED, CrowdActionStatusEnum.WAITING] } };
 @Injectable()
 export class UpdateCrowdactionStatusTask {
-    @Cron(CronExpression.EVERY_HOUR)
-    handleCron() {
-        Logger.log('[TaskScheduler] Task Scheduler is running - UpdateCrowdactionStatusTask');
+    constructor(private readonly CQRSHandler: ICQRSHandler) {}
 
-        // TODO: Implement
+    @Cron(CronExpression.EVERY_HOUR)
+    async handleCron() {
+        const crowdActionsList = await this.CQRSHandler.fetch(ListCrowdActionsQuery, { filter: FILTER });
+
+        let pageInfo = crowdActionsList.pageInfo;
+        let items = crowdActionsList.items;
+        let changedCrowdActions = 0;
+        for (let page = 1; page <= pageInfo.totalPages; page++) {
+            for (const crowdAction of items) {
+                const { id, status, joinStatus }: CrowdAction = CrowdAction.create(crowdAction).updateStatuses();
+                if (status !== crowdAction.status || joinStatus !== crowdAction.joinStatus) {
+                    if (status === CrowdActionStatusEnum.ENDED) {
+                        // TODO: Award Badges
+                        // this.CQRSHandler.execute(AwardBadgesForCrowdActionCommand, { crowdAction });
+                    }
+
+                    changedCrowdActions++;
+                    this.CQRSHandler.execute(UpdateCrowdActionStatusesCommand, { id, status, joinStatus });
+                }
+            }
+
+            if (pageInfo.totalPages > pageInfo.page) {
+                const result = await this.CQRSHandler.fetch(ListCrowdActionsQuery, {
+                    filter: FILTER,
+                    page: pageInfo.page + 1,
+                });
+
+                pageInfo = result.pageInfo;
+                items = result.items;
+            } else {
+                items = [];
+            }
+        }
+
+        Logger.log(`[TaskScheduler] UpdateCrowdactionStatusTask:Successfully - Executed on ${changedCrowdActions} CrowdActions`);
     }
 }
