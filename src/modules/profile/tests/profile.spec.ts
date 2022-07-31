fc25a34758f78c025e47c388c203af7f1b7d58f3
Michael Slattery <michaelslatterydev@gmail.com>
2022-07-31 16:04:08 +0100

    Replace controller with service and add debugger support

--- src/modules/profile/tests/profile.spec.ts
+++ src/modules/profile/tests/profile.spec.ts
@@ -1,27 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProfileController } from '@api/rest/profiles';
 import { ProfileService } from '../service';
 import { IProfileRepository, Profile } from '@domain/profile';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { Connection, connect, Model } from 'mongoose';
 import { ProfilePersistence, ProfileRepository, ProfileSchema } from '@infrastructure/mongo';
 import { getModelToken } from '@nestjs/mongoose';
-
-describe('ProfileController', () => {
-    let profileController: ProfileController;
+ 
+describe('ProfileService', () => {
+    let profileService: ProfileService;
     let mongod: MongoMemoryServer;
     let mongoConnection: Connection;
     let profileModel: Model<ProfilePersistence>;
 
     beforeEach(async () => {
-
         mongod = await MongoMemoryServer.create();
         const uri = mongod.getUri();
         mongoConnection = (await connect(uri)).connection;
         profileModel = mongoConnection.model(ProfilePersistence.name, ProfileSchema);
 
         const moduleRef: TestingModule = await Test.createTestingModule({
-            controllers: [ProfileController],
             providers: [
                 ProfileService,
                 { provide: getModelToken(ProfilePersistence.name), useValue: profileModel },
@@ -29,7 +26,7 @@ describe('ProfileController', () => {
             ],
         }).compile();
 
-        profileController = moduleRef.get<ProfileController>(ProfileController);
+        profileService = moduleRef.get<ProfileService>(ProfileService);
     });
 
     afterAll(async () => {
@@ -48,8 +45,8 @@ describe('ProfileController', () => {
 
     describe('findByIdOrFail', () => {
         it('should find a profile using an id or fail', async () => {
-            await new profileModel(ProfileStub()).save();
-            const profile = await profileController.getProfile(ProfileStub().id);
+            await (new profileModel(ProfileStub()).save());
+            const profile = await profileService.findByIdOrFail(ProfileStub().id);
             expect(profile.id).toBe(ProfileStub().id);
         });
     });
