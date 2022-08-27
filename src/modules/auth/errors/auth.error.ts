a33b91e8ceb8ce39279161230a93f460aba6399c
Mathias Mogensen <mathiasrieckm@gmail.com>
2022-08-27 21:00:24 +0200

    feat: make it possible to set statusCode in ApiError

--- src/modules/auth/errors/auth.error.ts
+++ src/modules/auth/errors/auth.error.ts
@@ -1,13 +1,14 @@
+import { HttpStatus } from '@nestjs/common';
 import { ApiError, ErrorCodesEnum } from '@common/types';
 
 export class AuthenticationError extends ApiError {
     constructor(message: string = 'Unauthorized') {
-        super({ message, code: ErrorCodesEnum.UNAUTHENTICATED });
+        super({ message, code: ErrorCodesEnum.UNAUTHENTICATED, statusCode: HttpStatus.UNAUTHORIZED });
     }
 }
 
 export class BadCredentialsError extends ApiError {
     constructor() {
-        super({ message: 'Bad creddentials' });
+        super({ message: 'Bad creddentials', statusCode: HttpStatus.UNAUTHORIZED });
     }
 }
