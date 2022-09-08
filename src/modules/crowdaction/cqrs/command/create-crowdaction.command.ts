bb14143544e0ae9712c865459f09b0cd73f1b1e7
Michael Slattery <michaelslatterydev@gmail.com>
2022-09-08 14:30:20 +0100

    Create Cron when crowdAction is made

--- src/modules/crowdaction/cqrs/command/create-crowdaction.command.ts
+++ src/modules/crowdaction/cqrs/command/create-crowdaction.command.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 import { ICQRSHandler, ICommand } from '@common/cqrs';
-import { CrowdActionJoinStatusEnum, CrowdActionStatusEnum, ICrowdActionRepository } from '@domain/crowdaction';
+import { CrowdAction, CrowdActionJoinStatusEnum, CrowdActionStatusEnum, ICrowdActionRepository } from '@domain/crowdaction';
 import {
     CategoryAndSubcategoryMustBeDisimilarError,
     CrowdActionMustBeInTheFutureError,
@@ -13,10 +15,15 @@ import { Identifiable } from '@domain/core';
 import { CreateCrowdActionDto } from '@infrastructure/crowdaction';
 import { GetCommitmentOptionsByType } from '@modules/commitmentoption';
 import { CommitmentOption } from '@domain/commitmentoption';
+import { UpdateCrowdActionStatusesCommand } from './update-crowdaction-statuses.command';
 
 @Injectable()
 export class CreateCrowdActionCommand implements ICommand {
-    constructor(private readonly crowdActionRepository: ICrowdActionRepository, private readonly cqrsHandler: ICQRSHandler) {}
+    constructor(
+        private readonly crowdActionRepository: ICrowdActionRepository,
+        private readonly CQRSHandler: ICQRSHandler,
+        private readonly schedulerRegistry: SchedulerRegistry,
+    ) {}
 
     async execute(data: CreateCrowdActionDto): Promise<Identifiable> {
         if (new Date() < data.startAt) {
@@ -45,10 +52,10 @@ export class CreateCrowdActionCommand implements ICommand {
             throw new CountryMustBeValidError(data.country);
         }
 
-        const commitmentOptions: CommitmentOption[] = await this.cqrsHandler.fetch(GetCommitmentOptionsByType, data.type);
+        const commitmentOptions: CommitmentOption[] = await this.CQRSHandler.fetch(GetCommitmentOptionsByType, data.type);
 
         const now = new Date();
-        return await this.crowdActionRepository.create({
+        const crowdAction = await this.crowdActionRepository.create({
             ...data,
             commitmentOptions,
             participantCount: 0,
@@ -57,5 +64,24 @@ export class CreateCrowdActionCommand implements ICommand {
             joinStatus: joinEndAt < now ? CrowdActionJoinStatusEnum.CLOSED : CrowdActionJoinStatusEnum.OPEN,
             status: data.startAt < now ? CrowdActionStatusEnum.STARTED : CrowdActionStatusEnum.WAITING,
         });
+
+        if (crowdAction) {
+            const job = new CronJob(crowdAction.endAt, () => {
+                const { status, joinStatus }: CrowdAction = crowdAction.updateStatuses();
+                if (status !== crowdAction.status || joinStatus !== crowdAction.joinStatus) {
+                    if (crowdAction.status === CrowdActionStatusEnum.ENDED) {
+                        // TODO: Award Badges
+                        // this.cqrsHandler.execute(AwardBadgesForCrowdActionCommand, { crowdAction });
+                    }
+                }
+
+                this.CQRSHandler.execute(UpdateCrowdActionStatusesCommand, crowdAction);
+            });
+
+            this.schedulerRegistry.addCronJob(crowdAction.id, job);
+            job.start();
+        }
+
+        return crowdAction;
     }
 }
