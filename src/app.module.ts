ea8ebae72fc0f432492e508046577ded657b83ec
Mathias Mogensencd collaction_website <mathiasrieckm@gmail.com>
2023-04-05 21:04:33 +0200

    refactor: scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
 import { ApiModule } from '@api/api.module';
 import { ModulesModule } from '@modules/modules.module';
 import { AuthModule } from '@modules/auth/auth.module';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
+import { SchedulerModule } from './modules';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        ScheduleModule.forRoot(),
+        SchedulerModule,
         ApiModule,
         AuthModule,
         InfrastructureModule,
--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -5,19 +5,9 @@ import { CrowdActionModule } from './crowdaction';
 import { ForumModule } from './forum';
 import { ParticipationModule } from './participation';
 import { ProfileModule } from './profile';
-import { StartupModule } from './startup';
 import { ThreadModule } from './thread';
 
 @Module({
-    imports: [
-        StartupModule,
-        CrowdActionModule,
-        ProfileModule,
-        ParticipationModule,
-        CommitmentModule,
-        ContactModule,
-        ForumModule,
-        ThreadModule,
-    ],
+    imports: [CrowdActionModule, ProfileModule, ParticipationModule, CommitmentModule, ContactModule, ForumModule, ThreadModule],
 })
 export class ModulesModule {}
