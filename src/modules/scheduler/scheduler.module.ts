90def717fdf8c192d50a8b638876de06980a862a
Mathias Mogensencd collaction_website <mathiasrieckm@gmail.com>
2023-04-05 21:04:33 +0200

    refactor: scheduler module

--- src/modules/scheduler/scheduler.module.ts
+++ src/modules/scheduler/scheduler.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CQRSModule } from '@common/cqrs';
 import { SchedulerService } from './scheduler.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-    imports: [CQRSModule],
+    imports: [ScheduleModule.forRoot(), CQRSModule],
     providers: [SchedulerService],
     exports: [SchedulerService],
 })
