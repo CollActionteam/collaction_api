721efbe8e8b0d51df662574744b187dc39983267
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2023-04-07 21:03:45 +0200

    Naming

--- azure/storage.tf
+++ azure/storage.tf
@@ -1,26 +1,5 @@
-# resource "azurerm_storage_account" "collaction" {
-#   name                     = "collaction"
-#   location                 = azurerm_resource_group.collaction_api.location
-#   resource_group_name      = azurerm_resource_group.collaction_api.name
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
-# 
-# resource "azurerm_storage_container" "collaction_api_dev" {
-#   name                  = "collaction-api-dev"
-#   storage_account_name  = azurerm_storage_account.collaction.name
-#   container_access_type = "blob"
-# }
-# 
-# resource "azurerm_storage_container" "collaction_api_production" {
-#   name                  = "collaction-api-production"
-#   storage_account_name  = azurerm_storage_account.collaction.name
-#   container_access_type = "blob"
-# }
-
-
 resource "azurerm_storage_account" "collaction_static_dev" {
-  name                     = "collaction-static-dev"
+  name                     = "collactionstaticdev"
   location                 = azurerm_resource_group.collaction_api.location
   resource_group_name      = azurerm_resource_group.collaction_api.name
   account_tier             = "Standard"
@@ -34,7 +13,7 @@ resource "azurerm_storage_container" "collaction_static_dev" {
 }
 
 resource "azurerm_storage_account" "collaction_static_production" {
-  name                     = "collaction-static-production"
+  name                     = "collactionstaticproduction"
   location                 = azurerm_resource_group.collaction_api.location
   resource_group_name      = azurerm_resource_group.collaction_api.name
   account_tier             = "Standard"
