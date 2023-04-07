0729177010b4c02e497840f1a03dbda660a4b2fd
Gilles Magalhaes <gilles.magalhaes@gmail.com>
2023-04-07 21:06:17 +0200

    Container registry admin enabled

--- azure/container_registry.tf
+++ azure/container_registry.tf
@@ -3,4 +3,5 @@ resource "azurerm_container_registry" "collaction" {
   resource_group_name = azurerm_resource_group.collaction_api.name
   location            = azurerm_resource_group.collaction_api.location
   sku                 = "Standard"
+  admin_enabled       = true
 }
