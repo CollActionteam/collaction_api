91f04ded18fd61794ac804c9b566ac74aca7c0ce
Michael Slattery <michaelslatterydev@gmail.com>
2023-04-23 12:26:27 +0900

    Fix issues with call not working

--- src/modules/forum/cqrs/query/fetch-forum-hierachy.query.ts
+++ src/modules/forum/cqrs/query/fetch-forum-hierachy.query.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { IQuery } from '@common/cqrs';
-import { IForum, IForumRepository } from '@domain/forum';
+import { Forum, IForum, IForumRepository } from '@domain/forum';
 
 @Injectable()
 export class GetForumHierarchy implements IQuery<string, IForum[]> {
     constructor(private readonly forumRepository: IForumRepository) {}
 
     async handle(forumId: string): Promise<IForum[]> {
-        return this.forumRepository.findAll({ parentList: [forumId] });
+        // For some reason only works if I add as any? Otherwise the compiler complains
+        const forums: Forum[] = await this.forumRepository.findAll({ parentList: { in: [forumId] } as any });
+        return forums;
     }
 }
