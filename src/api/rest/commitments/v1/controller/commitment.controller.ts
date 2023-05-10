19e564a21f556cfdd84399ed25ea4e51e5fb7ef4
Fredrick <fredabisai@gmail.com>
2023-05-10 22:37:30 +0300

    Implement a query to fetch all commitments

--- src/api/rest/commitments/v1/controller/commitment.controller.ts
+++ src/api/rest/commitments/v1/controller/commitment.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
+import {ApiBody, ApiOperation, ApiResponse, ApiTags} from '@nestjs/swagger';
 import { ICQRSHandler } from '@common/cqrs';
 import { CreateCommitmentCommand, DeleteCommitmentCommand, UpdateCommitmentCommand } from '@modules/commitment';
-import { Identifiable } from '@domain/core';
-import { CreateCommitmentDto, UpdateCommitmentDto } from '@infrastructure/commitment';
+import {Identifiable, IPaginatedList} from '@domain/core';
+import {CreateCommitmentDto, PaginatedCommitmentResponse, UpdateCommitmentDto} from '@infrastructure/commitment';
 import { FirebaseGuard } from '@modules/auth/decorators';
 import { UserRole } from '@domain/auth/enum';
+import {PaginationDto} from "@infrastructure/pagination";
+import {ListCommitmentsQuery} from "@modules/commitment/cqrs/query/list-commitments.query";
+import {ICommitment} from "@domain/commitment";
 
 @Controller('v1/commitments')
 @ApiTags('Commitment')
@@ -30,5 +33,19 @@ export class CommitmentController {
     @FirebaseGuard(UserRole.ADMIN)
     async updateCommitment(@Param('id') id: string, @Body() updateDto: UpdateCommitmentDto): Promise<Identifiable> {
         return await this.cqrsHandler.execute(UpdateCommitmentCommand, { id, updateDto });
+    }
+    @Get()
+    @ApiOperation({ summary: 'Retrieve a paginated list of Commitments' })
+    @ApiResponse({
+        status: 200,
+        description: 'Returns the found Commitments if any',
+        type: PaginatedCommitmentResponse,
+    })
+    @FirebaseGuard(UserRole.ADMIN)
+    async getAllCommitments(
+        @Query() pagination: PaginationDto,
+        @Query('tags') tags: string[],
+    ): Promise<IPaginatedList<ICommitment>> {
+        return this.cqrsHandler.fetch(ListCommitmentsQuery, { ...pagination, filter: { tags } });
     }
 }
