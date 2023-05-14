0afd0fa148c8aa500fc0b9ae65be2805b5c80a94
Fredrick <fredabisai@gmail.com>
2023-05-14 14:08:19 +0300

    Write tests for getting all commitments

--- src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
+++ src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
@@ -1,10 +1,19 @@
 import {ListCommitmentsQuery} from "@modules/commitment/cqrs/query/list-commitments.query";
 import {connect, Connection, Model} from "mongoose";
-import {CommitmentPersistence} from "@infrastructure/mongo";
+import {
+    CommitmentPersistence,
+    CommitmentSchema,
+} from "@infrastructure/mongo";
 import {MongoMemoryServer} from "mongodb-memory-server";
+import {Test} from "@nestjs/testing";
+import {CQRSHandler, CQRSModule, ICQRSHandler} from "@common/cqrs";
+import {getModelToken} from "@nestjs/mongoose";
+import {CreateCommitmentStub} from "@modules/commitment/cqrs/tests/create-commitment.command.spec";
+import {CreateCommitmentCommand} from "@modules/commitment";
 
 describe('ListCommitmentsQuery', () => {
-    const listCommitmentsQuery = ListCommitmentsQuery;
+    let listCommitmentsQuery: ListCommitmentsQuery;
+    let commitmentCommand: CreateCommitmentCommand;
     let CommitmentModel: Model<CommitmentPersistence>;
     let mongod: MongoMemoryServer;
     let mongoConnection: Connection;
@@ -12,5 +21,47 @@ describe('ListCommitmentsQuery', () => {
         mongod = await MongoMemoryServer.create();
         const uri = mongod.getUri();
         mongoConnection = (await connect(uri)).connection;
+        CommitmentModel = mongoConnection.model(CommitmentPersistence.name, CommitmentSchema);
+        const moduleRef = await Test.createTestingModule({
+            imports: [CQRSModule],
+            providers: [
+                ListCommitmentsQuery,
+                CreateCommitmentCommand,
+                { provide: ICQRSHandler, useClass: CQRSHandler },
+                { provide: getModelToken(CommitmentPersistence.name), useValue: CommitmentModel },
+            ],
+        }).compile();
+        listCommitmentsQuery = moduleRef.get<ListCommitmentsQuery>(ListCommitmentsQuery);
+        commitmentCommand = moduleRef.get<CreateCommitmentCommand>(CreateCommitmentCommand);
     })
+    afterAll(async () => {
+        await mongoConnection.dropDatabase();
+        await mongoConnection.close();
+        await mongod.stop();
+    });
+    afterEach(async () => {
+        const collections = mongoConnection.collections;
+        for (const key in collections) {
+            const collection = collections[key];
+            await collection.deleteMany({});
+        }
+    });
+    describe('getAllCommitmentsQuery', () => {
+        it('should find all  commitments', async () => {
+            const commitment = CreateCommitmentStub();
+            await commitmentCommand.execute(commitment);
+            const result = await listCommitmentsQuery.handle({
+                filter: {tags: ['FOOD']},
+                page: 1,
+                pageSize: 1
+            });
+            expect(result).toBeDefined();
+            expect(result?.items?.length).toEqual(1);
+            expect(result?.pageInfo?.pageSize).toEqual(1);
+            expect(result?.pageInfo?.totalPages).toEqual(1);
+            expect(result?.pageInfo?.totalItems).toEqual(1);
+
+        })
+    })
+
 })
