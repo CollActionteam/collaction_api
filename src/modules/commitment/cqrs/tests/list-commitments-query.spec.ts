188761f421dded6c561d63474407c49bf41a1325
Fredrick <fredabisai@gmail.com>
2023-05-14 20:54:43 +0300

    Format list commitments query spec imports(@nestjs/testing & mongodb-memory-server)

--- src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
+++ src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
@@ -1,9 +1,9 @@
 import { connect, Connection, Model } from 'mongoose';
 import { getModelToken } from '@nestjs/mongoose';
-import { ListCommitmentsQuery } from '@modules/commitment/cqrs/query/list-commitments.query';
-import { CommitmentPersistence, CommitmentSchema } from '@infrastructure/mongo';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { Test } from '@nestjs/testing';
+import { ListCommitmentsQuery } from '@modules/commitment/cqrs/query/list-commitments.query';
+import { CommitmentPersistence, CommitmentSchema } from '@infrastructure/mongo';
 import { CQRSHandler, CQRSModule, ICQRSHandler } from '@common/cqrs';
 import { CreateCommitmentStub } from '@modules/commitment/cqrs/tests/create-commitment.command.spec';
 import { CreateCommitmentCommand } from '@modules/commitment';
