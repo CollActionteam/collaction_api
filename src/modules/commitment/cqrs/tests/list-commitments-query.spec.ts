6f2458045a6cba419b33c13880e094a5465c47aa
Fredrick <fredabisai@gmail.com>
2023-05-14 12:54:41 +0300

    Make a Mongo DB connection

--- src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
+++ src/modules/commitment/cqrs/tests/list-commitments-query.spec.ts
@@ -1,8 +1,16 @@
 import {ListCommitmentsQuery} from "@modules/commitment/cqrs/query/list-commitments.query";
-import {Model} from "mongoose";
+import {connect, Connection, Model} from "mongoose";
 import {CommitmentPersistence} from "@infrastructure/mongo";
+import {MongoMemoryServer} from "mongodb-memory-server";
 
 describe('ListCommitmentsQuery', () => {
     const listCommitmentsQuery = ListCommitmentsQuery;
     let CommitmentModel: Model<CommitmentPersistence>;
+    let mongod: MongoMemoryServer;
+    let mongoConnection: Connection;
+    beforeAll(async () => {
+        mongod = await MongoMemoryServer.create();
+        const uri = mongod.getUri();
+        mongoConnection = (await connect(uri)).connection;
+    })
 })
